6dd71fa2e2ce3256991a3f5b792b96bd523d2e20
Dale <it@729ly.com>
2021-10-21 14:10:07 +0800

    up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,9 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
    <head>
       <meta charset="utf-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <title>Galerie Theme</title>
+      <title>良友开放平台</title>
       <meta name="HandheldFriendly" content="True" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="preload" href="https://galerie.eduardogomez.io/assets/css/app.css?v=bf041f893a" as="style" />
