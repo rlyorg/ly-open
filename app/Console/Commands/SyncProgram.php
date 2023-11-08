f871ac6c096c9a23001728c9ca1b7ff7dbfed649
XIANGKE GUO <mbp@XIANGKEs-MacBook-Pro.local>
2023-11-08 14:00:11 -0800

    update lymetas

--- app/Console/Commands/SyncProgram.php
+++ app/Console/Commands/SyncProgram.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
-use App\Models\Category;
+use App\Models\\Category;
 use App\Models\Program;
 
 class SyncProgram extends Command
@@ -75,6 +75,73 @@ class SyncProgram extends Command
                             'category_id' =>  $category->id,
                         ]); //同步更新
                     }
+                }
+
+                $newLyMetas = [
+                    [
+                        'code'=>'pm',
+                        'title'=>'天路男行客',
+                        'brief' => '男人的信仰园地',
+                    ],
+                    [
+                        'code'=>'sz',
+                        'title'=>'肋骨咏叹调',
+                        'brief' => '女性成长，心意更新',
+                    ],
+                    [
+                        'code'=>'tv',
+                        'title'=>'真爱世界',
+                        'brief' => '更好的过单身生活',
+                    ],
+                    [
+                        'code'=>'ym',
+                        'title'=>'颜明放羊班',
+                        'brief' => '快乐中与神相交',
+                    ],
+                    [
+                        'code'=>'fd',
+                        'title'=>'泛桌茶经班',
+                        'brief' => '以查代茶，以经解经',
+                    ],
+                    [
+                        'code'=>'hr',
+                        'title'=>'相约香草山',
+                        'brief' => '认识真理，更爱基督',
+                    ],
+                    [
+                        'code'=>'cfbwh',
+                        'title'=>'好想健康',
+                        'brief' => '病患与家属心声',
+                    ],
+                    [
+                        'code'=>'cedna',
+                        'title'=>'动漫查经员',
+                        'brief' => '从动漫看人生',
+                    ],
+                    [
+                        'code'=>'cfcbp',
+                        'title'=>'美丽见证‧生命大画家',
+                        'brief' => '上帝奇妙作为',
+                    ],
+                    [
+                        'code'=>'cfbls',
+                        'title'=>'抬头望四季',
+                        'brief' => '大自然与地域建筑',
+                    ],
+                    [
+                        'code'=>'cfbsg',
+                        'title'=>'Song Song声3',
+                        'brief' => '旋律背后的故事',
+                    ],
+                    [
+                        'code'=>'cgaal',
+                        'title'=>'爱情几岁',
+                        'brief' => '爱要成长',
+                    ],
+                ];
+                foreach ($newLyMetas as $lyMeta) {
+                    $program = Program::where('alias', $lyMeta['code'])->first();
+                    $program->update(['brief'=>$lyMeta['brief']]);
                 }
             }
         });
